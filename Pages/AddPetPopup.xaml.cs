ce0632749b9a8ce5c0e330966b312f0d0d037336
Liam <helm_of_protection@hotmail.com>
2025-02-01 20:32:18 -0800

    Added dynamic sizing to popup.

--- Pages/AddPetPopup.xaml.cs
+++ Pages/AddPetPopup.xaml.cs
@@ -7,6 +7,13 @@ public partial class AddPetPopup : Popup
     public AddPetPopup()
     {
         InitializeComponent();
+
+        var windowWidth = Application.Current.Windows[0].Width;
+        var windowHeight = Application.Current.Windows[0].Height;
+
+        // Set the size of the popup relative to the screen size
+        // For example, make the width 30% of the screen width and height 40% of the screen height
+        this.Size = new Size(windowWidth * 0.6, windowHeight * 0.6);
     }
 
     private void ClosePopup(object sender, EventArgs e)
